b09d3e3e17089e8dda66f0a3308b8db6f67396ca
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-05-12 10:13:23 +0900

    예제 6변 실습 삼각형모양 별 찍기 알고리즘

--- startree/Program.cs
+++ startree/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,14 @@ namespace startree
     {
         static void Main(string[] args)
         {
+            for(int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8-i; j++)
+                    Console.Write(" ");
+                for (int k = 0; k < i*2+1;k++)
+                    Console.Write("*");
+                Console.WriteLine("");
+            }
         }
     }
 }
